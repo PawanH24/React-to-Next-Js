a19f87d0e382f1a7a62b27069e9f8eb68db84cad
Pawan Hada <hadapawan13@gmail.com>
2026-02-19 09:58:36 +0545

    updated stats using derived state

--- travel-list/src/App.jsx
+++ travel-list/src/App.jsx
@@ -33,7 +33,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -51,7 +51,6 @@ function Form({ onAddItems }) {
 
     if (!description) return;
     const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log(newItem);
 
     onAddItems(newItem);
 
@@ -116,10 +115,24 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  const nItems = items.length;
+  const packedItems = items.filter((item) => item.packed).length;
+  const percent = Math.round((packedItems / nItems) * 100);
   return (
     <footer className="stats">
-      <em>💼 You have X items on your list, and you already packed X</em>
+      <em>
+        {percent === 100
+          ? "You got everything! Ready to go ✈"
+          : `💼 You have ${nItems} items on your list, and you already packed{" "}
+        ${packedItems} (${percent}%)`}
+      </em>
     </footer>
   );
 }
